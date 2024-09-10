f34ceddf9b2d9c10fab77513dd2b5ad4707057ac
Alexander Todorovic <127629967+alexandertodorovic@users.noreply.github.com>
2024-09-10 21:35:18 +0000

    Update times.py: Fix missing newline at end of file

--- times.py
+++ times.py
@@ -87,4 +87,3 @@ if __name__ == '__main__':
     print(get_current_time_in_japan())
     print(get_current_time_in_country('Europe/Berlin'))
     print(f"Time difference in hours: {calculate_time_difference()}")
-    
