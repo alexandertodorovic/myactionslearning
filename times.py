d25dd34d873f577228f1f7f8326361c8ea77afed
alexander todorovic <alexanderttodorovic@gmail.com>
2024-10-02 16:14:56 +0200

    Refactor print statements in times.py

--- times.py
+++ times.py
@@ -84,7 +84,7 @@ def calculate_time_difference():
     return time_difference_in_hours
 
 if __name__ == '__main__':
-    print(f"Current time in Japan: {get_current_time_in_japan()}")
-    print(f"Current time in Germany: {get_current_time_in_country('Europe/Berlin')}")
-    print(f"Time difference in hours: {calculate_time_difference()}")
+    print(f"Current time in Japan    => {get_current_time_in_japan()}")
+    print(f"Current time in Germany  => {get_current_time_in_country('Europe/Berlin')}")
+    print(f"Time difference in hours => {calculate_time_difference()}")
     
